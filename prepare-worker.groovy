c399b4b9a69f5cddbc86b8f56db225708058af68
merycicek <merycicek@gmail.com>
2020-09-02 15:53:15 +0000

    delete the master

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,4 +1,4 @@
-node ('master') {
+node  {
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
         stage ( 'Init') { 
             sh 'ssh -i $SSHKEY root@68.183.142.184 yum install epel-release -y'
