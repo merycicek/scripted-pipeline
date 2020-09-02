e5ea83f5bef3e5d8d62d8b0d3c0461b39887938e
merycicek <merycicek@gmail.com>
2020-09-02 15:49:06 +0000

    added new code

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,4 +1,9 @@
 node ('master') {
-    stage ( 'Init') { 
-        sh 'ssh -i /root/jenkins-master_rsa root@68.183.142.184 yum install epel-release -y'
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage ( 'Init') { 
+            sh 'ssh -i $SSHKEY root@68.183.142.184 yum install epel-release -y'
+        }    
+    }  
 }
+
+
