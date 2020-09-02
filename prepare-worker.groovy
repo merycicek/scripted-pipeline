8a3af8e4383d71bfb94e7b5798f6b2af2a8d7253
merycicek <merycicek@gmail.com>
2020-09-02 17:49:14 +0000

    updated the code

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -3,14 +3,14 @@ node  {
         
         
         stage ( 'Init') { 
-            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY root@68.183.142.184 yum install epel-release -y'
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@68.183.142.184 yum install epel-release -y'
         }    
     
         stage("Install git"){
-            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY root@68.183.142.184 yum install git -y'
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@68.183.142.184 yum install git -y'
         }
         stage("install Java"){
-            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY root@68.183.142.184 yum install java-1.8.0-openjdk-devel -y'
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@68.183.142.184 yum install java-1.8.0-openjdk-devel -y'
         }        
     }
 }
