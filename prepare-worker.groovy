93f39bfd2850695db496888a5d25886790483cf9
merycicek <merycicek@gmail.com>
2020-09-02 16:56:22 +0000

    added 3 satges

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,9 +1,16 @@
 node  {
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        
+        
         stage ( 'Init') { 
             sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY root@68.183.142.184 yum install epel-release -y'
         }    
-    }  
+    
+        stage("Install git"){
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY root@68.183.142.184 yum install git -y'
+        }
+        stage("install Java"){
+            sh 'ssh -o  StrictHostKeyChecking=no -i $SSHKEY root@68.183.142.184 yum install java-1.8.0-openjdk-devel -y'
+        }        
+    }
 }
-
-
