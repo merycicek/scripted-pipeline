d802a3e995b922e3ea36da082b4d56e18fe14e16
merycicek <merycicek@gmail.com>
2020-09-03 17:25:56 +0000

    added ansible melodi

---
+++ ansible-melodi.groovy
@@ -0,0 +1,35 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        ])
+    ])
+
+if (nodeIP?.trim()) {
+    node {
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+            stage('Pull Repo') {
+                git branch: 'release-1.0', changelog: false, poll: false, url: 'https://github.com/ikambarov/melodi.git'
+            }
+            stage("Install Apache"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install httpd -y'
+            }
+           
+            stage("Install git") {
+                sh 'scp -r -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} * /var/www/html/'
+            }
+            stage("Change Ownership"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} chown -R apache:apache /var/www/html/'
+            }
+            stage("Start Apache"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} systemctl start httpd && systemctl enable httpd'
+            }
+            stage("Install Ansible"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install ansible -y'
+            }
+           
+        }
+    }
+}
+else {
+    error 'Please enter valid IP address'
+}
