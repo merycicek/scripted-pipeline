94b6153620df8bc32fb05d0282befe34bd92ad20
merycicek <merycicek@gmail.com>
2020-09-05 11:58:14 +0000

    added new code

--- terraform-task.groovy
+++ terraform-task.groovy
@@ -1,15 +1,22 @@
-node {
-    stage( " Pull Repo") {
-       git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
+  
+node{
+    stage("Pull Repo"){
+        git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
     }
-    stage( " Terraform Init") {
-        sh '''
-            cd sandbox/
-            terraform-0.13 version
-            terraform-0.13 init
-           ''' 
-    }
-    stage( " Terraform Apply") {
-        sh 'echo "terraform apply" '
-    }
+
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        stage("Terrraform Init"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 version
+                terraform-0.13 init
+            '''
+        }
+        stage("Terraform Apply"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 apply -auto-approve
+            '''
+        }
+    }    
 }
